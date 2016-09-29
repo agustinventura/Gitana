3a2beb955481d53815c652d1987abc457cd30eb8
Agustín <agustinventura@gmail.com>
2016-09-29 17:45:30 +0200

    Initial access of all the issues in 2048.

--- extractor/init_db/config_db.py
+++ extractor/init_db/config_db.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 __author__ = 'valerio cosentino'
 
-PROJECT_NAME = "papyrus"
-DB_NAME = "db_papyrus" #"db_halflife" "db_2048"
-REPO_NAME = "db_papyrus" #"repo_halflife" "repo_2048"
-GIT_REPO_PATH = "C:\\Users\\atlanmod\\Desktop\\org.eclipse.papyrus" #"C:\\Users\\atlanmod\\Desktop\\halflife" "C:\\Users\\atlanmod\\Desktop\\2048"
+PROJECT_NAME = "2048"
+DB_NAME = "db_2048" #"db_halflife" "db_2048"
+REPO_NAME = "db_2048" #"repo_halflife" "repo_2048"
+GIT_REPO_PATH = "/home/agustin/Development/Python/Projects/2048" #"C:\\Users\\atlanmod\\Desktop\\halflife" "C:\\Users\\atlanmod\\Desktop\\2048"
 
 CONFIG = {
-            'user': 'root',
-            'password': 'root',
-            'host': 'localhost',
+            'user': 'gitana',
+            'password': 'gitana',
+            'host': '192.168.56.101',
             'port': '3306',
             'raise_on_warnings': False,
             'buffered': True
