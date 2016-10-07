eec4935bed9a5b7363e2bb44899d74d27454723b
Agustín <agustinventura@gmail.com>
2016-10-07 11:01:45 +0200

    Read all the issues

--- extractor/issue_tracker/github/querier_github.py
+++ extractor/issue_tracker/github/querier_github.py
@@ -1,15 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '...'
+__author__ = 'agustin ventura'
 
-# Data about project's issues should be obtained using the GitHub API through OAuth tokens.
-# https://help.github.com/articles/git-automation-with-oauth-tokens/
-# https://developer.github.com/v3/issues/
-# https://developer.github.com/v3/activity/events/types/ (types xii and xiii)
+import sys
+sys.path.insert(0, "..\\..")
 
-# In order to query the GitHub API through Python, you can use PyGitHub
-# https://pypi.python.org/pypi/PyGithub
-# http://pygithub.readthedocs.io/en/latest/introduction.html
+from github import Github
+from github import Repository
+import logging
+import logging.handlers
 
-#This script should contain the code to interact with PyGitHub.
+USERNAME = "gabrielecirulli"
+REPO_NAME = "2048"
 
+
+class GitHubQuerier:
+
+    def __init__(self, username, repo_name, logger):
+        self.logger = logger
+        self.username = username
+        self.repo_name = repo_name
+        self.github = None
+        self.repository = None
+
+    def load_repository(self):
+            try:
+                self.repository = self.github.get_repo(USERNAME+"/"+REPO_NAME)
+            except Exception, e:
+                self.logger.error("Error loading user " + USERNAME + " repository " + REPO_NAME + ": " + e.message)
+
+    def extract(self):
+        self.github = Github()
+        self.load_repository()
+        self.load_issues()
+
+    def load_issues(self):
+        if self.repository is not None:
+            last_page = self.get_last_page()
+            page_count = 0
+            while page_count <= last_page:
+                issues = self.get_all_issues_ascending().get_page(page_count)
+                for issue in issues:
+                    print issue
+                page_count += 1
+        else:
+            self.logger("Error loading issues, repository " + REPO_NAME + "does not exist")
+
+    def get_last_page(self):
+        last_page_url = self.get_all_issues_ascending()._getLastPageUrl()
+        last_page = int(last_page_url.split("page=")[-1])
+        return last_page
+
+    def get_all_issues_ascending(self):
+        return self.repository.get_issues(state="all", direction="asc")
+
+
+def main():
+    logger = get_stdout_logger()
+    github_querier = GitHubQuerier(USERNAME, REPO_NAME, logger)
+    github_querier.extract()
+
+
+def get_stdout_logger():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    return root
+
+
+if __name__ == "__main__":
+    main()
